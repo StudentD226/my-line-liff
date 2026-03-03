d56e629cb7355e94e81e972ce15755c9d56efb45
StudentD226 <nothingliff@gmail.com>
2026-03-03 12:38:20 +0000

    Update source code from local

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: ['profile.line-scdn.net'],
+  },
 };
 
 export default nextConfig;
